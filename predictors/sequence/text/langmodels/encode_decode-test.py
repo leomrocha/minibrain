4d96b09b4ecc2a644288620c1444c41f81462306
Leo Rocha <leo.m.rocha@gmail.com>
2019-11-06 12:46:11 +0100

    embeddings for utf-8 encoder and decoder class for the multihot advancing

--- predictors/sequence/text/langmodels/encode_decode-test.py
+++ predictors/sequence/text/langmodels/encode_decode-test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 # good that PyTorch v1.3.0+ has Transformers already implemented
@@ -6,6 +7,20 @@ from torch.nn.modules.transformer import TransformerEncoder, TransformerEncoderL
 from torch.nn.modules.transformer import TransformerDecoder, TransformerDecoderLayer
 import torch.nn.functional as F
 import faiss
+
+
+def _get_activation_fn(activation):
+    if activation == "sigmoid":
+        return F.sigmoid
+    elif activation == "tanh":
+        return F.tanh
+    elif activation == "relu":
+        return F.relu
+    elif activation == "gelu":
+        return F.gelu
+    else:
+        return None
+        # raise RuntimeError("activation should be sigmoid/tanh/relu/gelu, not %s." % activation)
 
 
 class UTF8Code(nn.Module):
@@ -40,16 +55,41 @@ class UTF8Code(nn.Module):
 
 
 class UTF8Embedding(nn.Module):
-    def __init__(self, utf8codebook, transpose=True):
+    def __init__(self, utf8codebook, transpose=False, lin_layers=(512, 512, 64, 32),
+                 activation=None, dropout=0.1):
         """
         Embedding Layer for UTF-8 based on pre-computed weights
+
+        If the linear layers are created the output size of the embedding is the one of the last linear layer.
+        By default there are no linear layers and the output of the embedding is the one given as input
+
         :param utf8codebook: Codebook containing the entire index to code matrix
+        :param transpose: if the output should be transposed such as if True the result is shape
+                          (batch size, embedding, sequence width) if False (default) shape is
+                          (batch size, sequence width, embedding)
+        :param lin_layers: iterable with size of the output of each linear layer, examples: [64], [256,64]
+        :param activation: activation type
+        :param dropout: for training dropout
         """
         self._transpose = transpose
         super(UTF8Embedding, self).__init__()
-        self.embeds = nn.Embedding(*utf8codebook.shape)
+        codebook_shape = utf8codebook.shape
+        self.embeds = nn.Embedding(*codebook_shape)
         self.embeds.weight.data.copy_(torch.from_numpy(utf8codebook))
-        self.embeds.weight.requires_grad_(False)
+        self.embeds.weight.requires_grad_(False)  # input embedding is fixed
+        self.lin = nn.ModuleList()
+        self.embedding = nn.ModuleList()
+        if lin_layers:
+            prev_dim = codebook_shape[1]
+            for dim in lin_layers:
+                lin = nn.Linear(prev_dim, dim)
+                dropout = nn.Dropout(dropout)
+                prev_dim = dim
+                self.lin.extend([lin, dropout])
+
+        self.embedding.append(self.embeds)
+        self.embedding.extend(self.lin)
+        self.activation = _get_activation_fn(activation)
 
     @property
     def transpose(self):
@@ -61,9 +101,11 @@ class UTF8Embedding(nn.Module):
 
     def forward(self, x):
         # (batch size, sequence-width)
-        ret = self.embeds(x)  # (batch size, sequence width[values]) -> # (batch size, sequence width, embedding)
+        ret = self.embedding(x)  # (batch size, sequence width[values]) -> # (batch size, sequence width, embedding)
         if self._transpose:
             ret = ret.transpose(1, 2)  # (batch size, embedding, sequence width)
+        if self.activation:
+            ret = self.activation(ret)
         return ret
 
 
@@ -108,37 +150,73 @@ class UTF8AttentionalEmbedding(UTF8Embedding):
         return ret
 
 
-class UTF8Decoder(nn.Module):
-    def __init__(self, utf8codebook):
-        # dimension would be:
-        # 1024,
-        super(UTF8Decoder, self).__init__()
-        self.embeds = nn.Embedding(*utf8codebook.shape)
-        self.embeds.weight.data.copy_(torch.from_numpy(utf8codebook))
-        self.embeds.weight.requires_grad(False)
-
-    pass
-
-
 class UTF8DecoderMultihot(nn.Module):
-    def __init__(self, utf8codebook, lin_layers=[None], seg_indices=(0, 4, 256+4, 64+256+4, 2*64 + 256+4)):
+    def __init__(self, utf8codebook_shape, lin_layers=(32, 512, 512), activation="gelu",
+                 seg_indices=(0, 4, 256+4, 64+256+4, 2*64 + 256+4, 3*64 + 256+4), dropout=0.1):
         """
         This modules decodes a given code, if the code_dim is given (an iterable not None) then a MLP decoder is created to adapt
         from the input dimension to the utf8codebook dimension
-        :param utf8codebook:
+        :param utf8codebook_shape:  shape of the pre-computed codebook used
         :param lin_layers: if not None, creates a MLP with the given dimensions  for example[512, 512, 388] and after that
         a multiple Softmax is used for each segment on the code
-        :param seg_indices: indices of the segments on the multihot code
+        :param activation: intermediate activation function
+        :param seg_indices: START and END indices of the segments on the multihot code, default for 4 segments code
+        for 1 segment1 is: seg_indices=(0, 4, 256+4)
+        for 2 segments is: seg_indices=(0, 4, 256+4, 64+256+4)
+        for 3 segments is: seg_indices=(0, 4, 256+4, 64+256+4, 2*64 + 256+4)
+        for 4 segments is: seg_indices=(0, 4, 256+4, 64+256+4, 2*64 + 256+4, 3*64 + 256+4)
+        but with the default value works for every segment encoding as it has a verification, so don't touch
         """
-        # dimension would be:
-        # 1024,
-        super(UTF8Decoder, self).__init__()
-        self.embeds = nn.Embedding(*utf8codebook.shape)
-        self.embeds.weight.data.copy_(torch.from_numpy(utf8codebook))
-        self.embeds.weight.requires_grad(False)
+        super(UTF8DecoderMultihot, self).__init__()
+        # self.embeds = nn.Embedding(*utf8codebook.shape)
+        # self.embeds.weight.data.copy_(torch.from_numpy(utf8codebook))
+        # self.embeds.weight.requires_grad(False)
+        # the decoder has to pass from a dimension that compresses all the information to several vectors that
+        # each encode one part of the output that later will be mapped by the utf8codebook to an int
+        # the mechanism is:
+        # input -> adaptation to utf8codebook_shape by linear layers ->
+        #       -> separation in segments ->
+        #       -> Softmax ->
+        #       -> concatenation
+        # And later must be decoded to an integer index. This decoding to int
+        # will be done in another class as it's a work on it's own and better separate it
 
-    pass
+        # Linear layers to adapt input latent to the codebook dimension
+        self.lin = nn.ModuleList()
+        if lin_layers:
+            lin_layers = list(lin_layers) + [utf8codebook_shape[1]]  # add dimension
+            for i in range(len(lin_layers)-1):
+                lin = nn.Linear(lin_layers[i], lin_layers[i+1])
+                drop = nn.Dropout(dropout)
+                self.lin.extend([lin, drop])
+        self.activation = _get_activation_fn(activation)
+        # Linear layers to adapt dimension for the separated softmax
+        sidx = np.array(seg_indices)
+        sidx = sidx[sidx <= utf8codebook_shape[1]]  # filter out unused segments (dimension)
+        ridx = np.roll(sidx, shift=-1)  # get the end index of each segment
+        dims = ridx - sidx   # get segment dimensions
+        dims = dims[0 < dims]  # this takes out the unused segments
+        #
+        self.segments = nn.ModuleList()  # the order is important
+        for d in dims:
+            # the order is important
+            lin = nn.Linear(utf8codebook_shape[1], d)
+            sm = nn.Softmax()
+            self.segments.extend([lin, sm])
 
+    def forward(self, x):
+        # assumes input shape as:  (batch size, sequence width, embedding)
+        # adapt input dimension to codebook
+        ret = self.lin(x)
+        if self.activation:
+            ret = self.activation(ret)
+        # process every part of the segment
+        segments = []
+        for net in self.segments:
+            segments.append(net(ret))
+        # concatenate results in axis to get to the embedding size:
+        res = torch.cat(segments, dim=-1)
+        return res
 
 
 class EncodeDecodeTest(nn.Module):
